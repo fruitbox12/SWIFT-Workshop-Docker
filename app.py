9063f6e89a324dd5c9328be4d32a83004c40e3c9
Dylan <dylanwong007@gmail.com>
2021-04-23 15:46:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
+    return 'Hey, we have Flask in a Docker container!'
+
     """Reverse and return the provided URI"""
     return "".join(reversed(random_string))
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
